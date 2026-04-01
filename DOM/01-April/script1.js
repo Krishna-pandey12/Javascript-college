607f53028a89fe79c9fb97f96eb9b8d277d14265
krishna pandey <krishna7376285419@gmail.com>
2026-04-01 09:44:16 +0530

    01-April-2026

---
+++ DOM/01-April/script1.js
@@ -0,0 +1,4 @@
+console.dir(document.head);
+let div1=document.getElementById("mydiv");
+console.log(div1);
+div1.innerHTML="Explanation of html";
---
+++ DOM/script.js
@@ -0,0 +1,4 @@
+let myDiv=document.querySelector("div");
+console.log(myDiv.getAttribute("id"));
+myDiv.style.backgroundColor="red";
+myDiv.style.height="20px;                                               
