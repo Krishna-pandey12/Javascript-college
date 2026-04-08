c1a576f8ec8ad1131452b7d0f7441222c86cf32d
krishna pandey <krishna7376285419@gmail.com>
2026-04-08 10:09:26 +0530

    08-April-2026

--- DOM/01-April/script1.js
+++ DOM/01-April/script1.js
@@ -1,4 +1,18 @@
-console.dir(document.head);
-let div1=document.getElementById("mydiv");
-console.log(div1);
-div1.innerHTML="Explanation of html";
+// // console.dir(document.head);
+// // let div1=document.getElementById("mydiv");
+// // console.log(div1);
+// // div1.innerHTML="Explanation of html";
+// let p1=document.getElementsByName('p');
+// console.log(p1);
+// Select using querySelector
+    let text = document.querySelector("#myDiv");  
+    let para = document.querySelector(".p1");     
+    let button = document.querySelector("#btn");   
+
+    // Apply action
+    button.addEventListener("click", () => {
+      text.textContent = "DOM Updated ";
+      text.style.color = "red";
+
+      para.style.color = "blue";
+    });
